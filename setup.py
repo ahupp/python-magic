7db98f44bccc77c3adaef638b7765aaf0f6230ae
Adam Hupp <adam@hupp.org>
2010-03-31 15:50:55 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, Extension
+#from distutils.core import setup, Extension
+
+setup(name='python-magic',
+      description='File type identification using libmagic',
+      author='Adam Hupp',
+      author_email='adam@hupp.org',
+      url="http://hupp.org/adam/hg/python-magic",
+      version='0.1',
+      py_modules=['magic'],
+      long_description="""This module uses ctypes to access the libmagic file type
+identification library.  It makes use of the local magic database and
+supports both textual and MIME-type output.
+""",
+      keywords="mime magic file",
+      license="PSF",
+      )
---
+++ test.py
@@ -0,0 +1,51 @@
+
+import os.path
+import unittest
+import random
+from StringIO import StringIO
+from os import path
+from magic import Magic, MagicException
+
+testfile = [
+    ("magic.pyc", "python 2.4 byte-compiled", "application/octet-stream"),
+    ("test.pdf", "PDF document, version 1.2", "application/pdf"),
+    ("test.gz", 'gzip compressed data, was "test", from Unix, last modified: '
+     'Sat Jun 28 18:32:52 2008', "application/x-gzip"),
+    ("text.txt", "ASCII text", "text/plain; charset=us-ascii"),
+    ]
+
+
+class TestMagic(unittest.TestCase):
+
+    mime = False
+    
+    def setUp(self):
+        self.m = Magic(mime=self.mime)
+
+    def testFileTypes(self):
+        for filename, desc, mime in testfile:
+            filename = path.join(path.dirname(__file__),
+                                 "testdata",
+                                 filename)
+            if self.mime:
+                target = mime
+            else:
+                target = desc
+                
+            self.assertEqual(target, self.m.from_buffer(open(filename).read(1024)))
+            self.assertEqual(target, self.m.from_file(filename), filename)
+        
+
+    def testErrors(self):
+        self.assertRaises(IOError, self.m.from_file, "nonexistent")
+        self.assertRaises(MagicException, Magic, magic_file="noneexistent")
+        os.environ['MAGIC'] = '/nonexistetn'
+        self.assertRaises(MagicException, Magic)
+        del os.environ['MAGIC']
+
+class TestMagicMime(TestMagic):
+    mime = True
+        
+if __name__ == '__main__':
+    unittest.main()
+    
