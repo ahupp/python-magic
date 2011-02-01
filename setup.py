a75cf0a4a7790eb106155c947af9612f15693b6e
Adam Hupp <adam@hupp.org>
2011-01-31 22:51:03 -0800

    Remove ctypes setup() dependency, already included in python >= 2.5 and it breaks pypi installs.

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(name='python-magic',
       url="http://github.com/ahupp/python-magic",
       version='0.4.0',
       py_modules=['magic'],
-      install_requires=['ctypes'],
       long_description="""This module uses ctypes to access the libmagic file type
 identification library.  It makes use of the local magic database and
 supports both textual and MIME-type output.
