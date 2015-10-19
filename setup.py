569467026d286667803a1a83195dd4d6c3aa5787
Adam Hupp <adam@hupp.org>
2015-10-19 09:23:18 -0700

    bump to 0.4.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='python-magic',
       author='Adam Hupp',
       author_email='adam@hupp.org',
       url="http://github.com/ahupp/python-magic",
-      version='0.4.7',
+      version='0.4.8',
       py_modules=['magic'],
       long_description="""This module uses ctypes to access the libmagic file type
 identification library.  It makes use of the local magic database and
@@ -19,11 +19,5 @@ supports both textual and MIME-type output.
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
       ],
       )
