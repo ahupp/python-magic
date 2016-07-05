cb18a2a77cabeb299e26726ec3ea0df4ec85386b
Adam Hupp <adam@hupp.org>
2016-07-04 19:58:00 -0700

    bump to 0.4.12

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='python-magic',
       author='Adam Hupp',
       author_email='adam@hupp.org',
       url="http://github.com/ahupp/python-magic",
-      version='0.4.11',
+      version='0.4.12',
       py_modules=['magic'],
       long_description="""This module uses ctypes to access the libmagic file type
 identification library.  It makes use of the local magic database and
