9af943e0564c662f3e99cc4ee86363b8df1aff46
Adam Hupp <adam@hupp.org>
2018-08-16 08:08:50 -0700

    fixes to run.sh

--- test/run.sh
+++ test/run.sh
@@ -7,13 +7,13 @@ export LC_ALL=en_US.UTF-8
 THISDIR=`dirname $0`
 export PYTHONPATH=${THISDIR}/..
 
-PYTHONS="python2.7 python3.5 bad"
+PYTHONS="python2.7 python3.5"
 
 for pyver in $PYTHONS; do
     if which $pyver > /dev/null; then
+        echo "found $pyver"
         $pyver ${THISDIR}/test.py
     else
         echo "version $pyver not found"
-
     fi
 done
