bd6dd7ccaed477bf1f6f675f7ec13ccbf4383d90
Hugo <hugovk@users.noreply.github.com>
2020-01-04 23:26:46 +0200

    Add support for Python 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(name='python-magic',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: Implementation :: CPython',
       ],
       )
