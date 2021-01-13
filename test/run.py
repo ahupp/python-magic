4a810b4f30f161b889d59cf2a207820fc0c731b7
SchoolGuy <matrixfueller@gmail.com>
2021-01-13 09:12:57 +0100

    Remove Python2 from the tests

--- test/run.py
+++ test/run.py
@@ -29,5 +29,5 @@ def run_test(versions):
     if not found:
         sys.exit("No versions found: " + str(versions))
 
-run_test(["python2", "python2.7"])
+
 run_test(["python3.5", "python3.6", "python3.7", "python3.8"])
